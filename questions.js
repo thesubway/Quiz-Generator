4df2277ca04ee5a9177f24cf03d8c00802425a05
Daniel Hoang <thesubway@gmail.com>
2015-05-04 17:13:54 -0700

    Add a few questions.

---
+++ questions.js
@@ -0,0 +1,30 @@
+var allOfAbove = "All of the above";
+var noneOfAbove = "None of the above";
+var nUndefined = "n is undefined";
+var noSolution = "no solution";
+var mathQuestions = [
+	{
+		question:"(2 ^ 2) = n. What is n?",
+		questionType: 1,
+		choices: ["same as 2+2","same as2*2","same as 2^2",allOfAbove],
+		correctAns: allOfAbove,
+	},
+	{
+		question:"What does 0 ^ 0 equal?",
+		questionType: 1,
+		choices: ["same as 0+0","same as 0*0","1",nUndefined],
+		correctAns: nUndefined,
+	},
+	{
+		question:"What does 1 + 1 equal?",
+		questionType: 1,
+		choices: ["window","11","infinity",noSolution],
+		correctAns: "window",
+	},
+	{
+		question:"How many acute angles are in a rectangle?",
+		questionType: 1,
+		choices: ["0","1","2","3","4"],
+		correctAns: "0",
+	}
+];
