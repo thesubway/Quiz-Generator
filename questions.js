bfa13a1f860c5a4b165909e031e136aa727ef834
Daniel Hoang <thesubway@gmail.com>
2015-05-09 20:24:53 -0700

    Add science and history questions to js file.

--- questions.js
+++ questions.js
@@ -2,6 +2,7 @@ var allOfAbove = "All of the above";
 var noneOfAbove = "None of the above";
 var nUndefined = "n is undefined";
 var noSolution = "no solution";
+var subjects = ["Math","History","Science"];
 var mathQuestions = [
 	{
 		question:"(2 ^ 2) = n. What is n?",
@@ -32,5 +33,69 @@ var mathQuestions = [
 		questionType: 1,
 		choices: ["-1","1","-4","4"],
 		correctAns: "4",
+	}
+];
+var historyQuestions = [
+	{
+		question:"Which war was first?",
+		questionType: 1,
+		choices: ["World War I","World War II"],
+		correctAns: "World War I",
+	},
+	{
+		question:"World War II ended when Germany surrendured.",
+		questionType: 1,
+		choices: ["True","False"],
+		correctAns: "False",
+	},
+	{
+		question:"World War II ended when Japan surrendured.",
+		questionType: 1,
+		choices: ["True","False"],
+		correctAns: "True",
+	},
+	{
+		question:"Who did the US fight in the revolutionary war?",
+		questionType: 1,
+		choices: ["Britain","France","The South"],
+		correctAns: "Britain",
+	},
+	{
+		question:"Anne Frank and Martin Luther King were born the same year.",
+		questionType: 1,
+		hoices: ["True","False"],
+		correctAns: "True",
+	}
+];
+var chemQuestions = [
+	{
+		question:"What is O?",
+		questionType: 1,
+		choices: ["Zero","Oxygen","Organics",allOfAbove],
+		correctAns: "Oxygen",
+	},
+	{
+		question:"Which planet has human life?",
+		questionType: 1,
+		choices: ["The Sun","Venus","Earth",allOfAbove],
+		correctAns: "Earth",
+	},
+	{
+		question:"Which one of these live in the water?",
+		questionType: 1,
+		choices: ["Dog","Human","Whale","Koala"],
+		correctAns: "Whale",
+	},
+	{
+		question:"What is a change from liquid to solid?",
+		questionType: 1,
+		choices: ["condensation","boiling","melting","freezing"],
+		correctAns: "freezing",
+	},
+	{
+		question:"Natural selection is based on:",
+		questionType: 1,
+		choices: ["Survival of the fittest","Attractive","Genetic Engineering"],
+		correctAns: "Survival of the fittest",
 	}
 ];
